0fcb2744cc2c50465180d176bf8e305882843ed4
mrsmarah <marahabdallah91@gmail.com>
2020-06-01 03:10:56 +0300

    finish6

--- __tests__/linked-list.test.js
+++ __tests__/linked-list.test.js
@@ -17,14 +17,14 @@ describe('Linked List', () => {
     const linkedListInstance = new linkedListModule();
     linkedListInstance.insert(6); //head
     linkedListInstance.insert(5);
-    expect(linkedListInstance.head.value).toStrictEqual(6);
+    expect(linkedListInstance.head.value).toStrictEqual(5);
   });
   it('4. Can properly insert multiple nodes into the linked list', () => {
     const linkedListInstance = new linkedListModule();
     linkedListInstance.insert(5);
     linkedListInstance.insert(3);
     linkedListInstance.insert(1);
-    expect(linkedListInstance.head.next.next.value).toStrictEqual(1);
+    expect(linkedListInstance.head.next.next.value).toStrictEqual(5);
 
   });
   it('5. Will return true when finding a value within the linked list that exists', () => {
@@ -48,6 +48,54 @@ describe('Linked List', () => {
     linkedListInstance.insert(5);
     linkedListInstance.insert(3);
     linkedListInstance.insert(1);
+    expect(linkedListInstance.toString()).toStrictEqual('{ 1 } -> { 3 } -> { 5 } -> { 7 } -> { 9 } -> { null }');
+  });
+  it('8. Can successfully add a node to the end of the linked list', () => {
+    const linkedListInstance = new linkedListModule();
+    linkedListInstance.append(6); 
+    linkedListInstance.append(5); 
+    expect(linkedListInstance.head.next.value).toStrictEqual(5);
+  });
+  it('9. Can successfully add multiple nodes to the end of a linked list', () => {
+    const linkedListInstance = new linkedListModule();
+    linkedListInstance.append(9);
+    linkedListInstance.append(7);
+    linkedListInstance.append(5);
+    linkedListInstance.append(3);
+    linkedListInstance.append(1);
     expect(linkedListInstance.toString()).toStrictEqual('{ 9 } -> { 7 } -> { 5 } -> { 3 } -> { 1 } -> { null }');
   });
-}); 
+  it('10. Can successfully insert a node before a node located i the middle of a linked list', () => {
+    const linkedListInstance = new linkedListModule();
+    linkedListInstance.append(1);
+    linkedListInstance.append(2);
+    linkedListInstance.append(4);
+    linkedListInstance.append(5);
+    linkedListInstance.insertBefore(4,3);
+    expect(linkedListInstance.toString()).toStrictEqual('{ 1 } -> { 2 } -> { 3 } -> { 4 } -> { 5 } -> { null }');
+  });
+  it('11. Can successfully insert a node before the first node of a linked list', () => {
+    const linkedListInstance = new linkedListModule();
+    linkedListInstance.append(9);
+    linkedListInstance.append(8);
+    linkedListInstance.insertBefore(9,10);
+    expect(linkedListInstance.toString()).toStrictEqual('{ 10 } -> { 9 } -> { 8 } -> { null }');
+  });
+  it('12.Can successfully insert after a node in the middle of the linked list', () => {
+    const linkedListInstance = new linkedListModule();
+    linkedListInstance.append(1);
+    linkedListInstance.append(2);
+    linkedListInstance.append(4);
+    linkedListInstance.append(5);
+    linkedListInstance.insertAfter(2,3);
+    expect(linkedListInstance.toString()).toStrictEqual('{ 1 } -> { 2 } -> { 3 } -> { 4 } -> { 5 } -> { null }');
+  });
+  it('13. Can successfully insert a node after the last node of the linked list', () => {
+    const linkedListInstance = new linkedListModule();
+    linkedListInstance.append(10);
+    linkedListInstance.append(9);
+    linkedListInstance.insertAfter(9,8);
+    expect(linkedListInstance.toString()).toStrictEqual('{ 10 } -> { 9 } -> { 8 } -> { null }');
+  });
+
+});
--- challenges/Data-Structures/linked-list.js
+++ challenges/Data-Structures/linked-list.js
@@ -13,18 +13,15 @@ class LinkedList {
   }
 
   insert(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-      return this;
-    }   
-    let current = this.head;
-    while (current.next){
-      current = current.next;
+    const node = new Node(value, this.head);
+    if (!this.head){
+     this.head = node;
+    } else {
+     node.next = this.head;
+     this.head = node;
     }
-    current.next = newNode;
-    return this;
-  }
+   return this;
+   }
 
   includes(value){
     let currentNode = this.head;
@@ -40,14 +37,78 @@ class LinkedList {
 
   toString() {
     let current = this.head;
-    let string = '';
-    string = `{ ${current.value} }`;
+    let string = `{ ${current.value} }`;
     while (current.next){
       current = current.next;
       string = string + ` -> { ${current.value} }`;
+
     }
-    return string + ` -> { ${current.next} }`;
+    return string + ` -> { null }`;
+    console.log(string);
   }
+
+  append(value){
+    let node = new Node(value);
+    //if empty make head(edge case)
+    if (!this.head) {
+        this.head = node;
+        return this;
+        }
+    //insert at last
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return this;
+    }
+
+  insertBefore(value, newVal) {
+      //if empty return nothing
+      if (!this.head) return;
+      //if the value is head use insert()
+      let node = new Node(newVal);
+      let currentNode = this.head;
+      if(currentNode.value === value) return this.insert(newVal);
+      //if the value is any other node
+      while (currentNode.next) {
+      if (currentNode.next.value === value) {
+          node.next = currentNode.next;
+          currentNode.next = node;
+          return this;
+      }
+      currentNode = currentNode.next;
+      }
+      return ;
+  }  
+
+  insertAfter(value, newVal) {
+      //if empty return nothing
+      if (!this.head) return;
+      //if the value is any other node 
+      let node = new Node(newVal);
+      let currentNode = this.head;
+      while (currentNode.next) {
+          currentNode = currentNode.next;
+          if (currentNode.value === value) {
+              node.next = currentNode.next;
+              currentNode.next = node;
+              return this;
+          }
+      }
+      return ;
+  } 
 }
+
+ 
+
+// const ll = new LinkedList();
+// ll.insert(100);
+// ll.insert(200);
+// ll.insert(300);
+// ll.append(0);
+// ll.insertAfter(200,100000);
+
+// ll.toString();
 
 module.exports = LinkedList;
