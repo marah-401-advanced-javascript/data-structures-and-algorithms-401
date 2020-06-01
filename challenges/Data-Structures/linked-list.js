2347795d810e393bcb21368f320988d4a0290e41
mrsmarah <marahabdallah91@gmail.com>
2020-06-01 14:43:08 +0300

    fix lint

--- challenges/Data-Structures/linked-list.js
+++ challenges/Data-Structures/linked-list.js
@@ -15,13 +15,13 @@ class LinkedList {
   insert(value) {
     const node = new Node(value, this.head);
     if (!this.head){
-     this.head = node;
+      this.head = node;
     } else {
-     node.next = this.head;
-     this.head = node;
+      node.next = this.head;
+      this.head = node;
     }
-   return this;
-   }
+    return this;
+  }
 
   includes(value){
     let currentNode = this.head;
@@ -44,16 +44,16 @@ class LinkedList {
 
     }
     return string + ` -> { null }`;
-    console.log(string);
+    // console.log(string);
   }
 
   append(value){
     let node = new Node(value);
     //if empty make head(edge case)
     if (!this.head) {
-        this.head = node;
-        return this;
-        }
+      this.head = node;
+      return this;
+    }
     //insert at last
     let currentNode = this.head;
     while (currentNode.next) {
@@ -61,42 +61,42 @@ class LinkedList {
     }
     currentNode.next = node;
     return this;
-    }
+  }
 
   insertBefore(value, newVal) {
-      //if empty return nothing
-      if (!this.head) return;
-      //if the value is head use insert()
-      let node = new Node(newVal);
-      let currentNode = this.head;
-      if(currentNode.value === value) return this.insert(newVal);
-      //if the value is any other node
-      while (currentNode.next) {
+    //if empty return nothing
+    if (!this.head) return;
+    //if the value is head use insert()
+    let node = new Node(newVal);
+    let currentNode = this.head;
+    if(currentNode.value === value) return this.insert(newVal);
+    //if the value is any other node
+    while (currentNode.next) {
       if (currentNode.next.value === value) {
-          node.next = currentNode.next;
-          currentNode.next = node;
-          return this;
+        node.next = currentNode.next;
+        currentNode.next = node;
+        return this;
       }
       currentNode = currentNode.next;
-      }
-      return ;
+    }
+    return ;
   }  
 
   insertAfter(value, newVal) {
-      //if empty return nothing
-      if (!this.head) return;
-      //if the value is any other node 
-      let node = new Node(newVal);
-      let currentNode = this.head;
-      while (currentNode.next) {
-          currentNode = currentNode.next;
-          if (currentNode.value === value) {
-              node.next = currentNode.next;
-              currentNode.next = node;
-              return this;
-          }
+    //if empty return nothing
+    if (!this.head) return;
+    //if the value is any other node 
+    let node = new Node(newVal);
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      if (currentNode.value === value) {
+        node.next = currentNode.next;
+        currentNode.next = node;
+        return this;
       }
-      return ;
+    }
+    return ;
   } 
 }
 
