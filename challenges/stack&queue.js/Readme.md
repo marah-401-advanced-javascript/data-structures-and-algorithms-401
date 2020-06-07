c03061e35f866769a5589bf0657addc7a0bdeb35
mrsmarah <marahabdallah91@gmail.com>
2020-06-07 03:02:37 +0300

    10

---
+++ challenges/stack&queue.js/Readme.md
@@ -0,0 +1,30 @@
+# linked list
+
+- Implementation of the stack and queue.
+
+
+
+## Challenge
+
+Create a node ,stack, queue classes with these methods:
+
+- push() >>> stack
+- pop() >>> stack
+- peek() >>> stack-queue
+- isEmpty() >>> stack-queue
+- enqueue() >>> queue
+- dequeue() >>> queue
+
+## Approach & Efficiency
+
+- Created a Node class.
+- stack with all the required methods.
+- queue with all the required methods.
+
+## Solution
+
+![stack](/assets/ch10s.jpg)
+![queue](/assets/ch10q.jpg)
+
+
+
---
+++ challenges/stack&queue.js/stack-and-queue.js
@@ -0,0 +1,96 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.storage = [];
+    this.top = null;
+    
+  }
+  push(val) {
+    let node = new Node(val);
+    if (this.top) {
+      this.storage.unshift(node);
+      this.storage[1].next = this.top.value;
+      // this.top.next =this.storage.length-1;
+      this.top = node;
+      
+    } else {
+      this.storage.unshift(node);
+      this.top = node;
+    }
+  }
+  
+  peek() {
+    return this.top;
+  }
+  
+  pop() {
+    const removed = this.storage.shift();
+    this.top = this.storage[0] ? this.storage[0] : null;
+    return removed;
+  }
+  isEmpty(){
+    if(this.top){
+      return false;
+    }else {return true;}
+  }
+}
+
+let myStack = new Stack;
+
+myStack.push(0);
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
+myStack.peek();
+myStack.pop();
+
+console.log(myStack,
+);
+
+class Queue {
+  constructor() {
+    this.storage=[];
+    // this.front = null;
+  }
+  enqueue(val) {
+    let node = new Node(val);
+    this.storage.push(node);
+    // this.front = node;
+  }
+  dequeue() {
+    return this.storage.shift();
+  }
+
+  peek() {
+    return this.storage[0]?this.storage[0]:'exception';
+    // return this.front?this.front:'exception';
+
+  }
+  isEmpty(){
+    if(this.front){
+      return false;
+    }else {return true;}
+  }
+}
+
+
+let myQueue= new Queue;
+
+myQueue.enqueue(0);
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+// myQueue.dequeue();
+// myStack.pop();
+
+// console.log(myQueue);
+console.log(myQueue.dequeue());
+console.log(myQueue);
