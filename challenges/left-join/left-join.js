9604761e9a48126cca43e5062d9a174e1b0825b2
marah <marahabdallah91@gmail.com>
2020-07-08 00:56:23 +0300

    new br

---
+++ challenges/left-join/left-join.js
@@ -0,0 +1,99 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+  
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  prepend(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+  
+}
+  
+  
+class Hashmap {
+    
+  constructor(size) {
+    this.size = size;
+    this.storage = new Array(size); // [] with that size
+  }
+  
+  hash(key) {
+    const sumCharCode = key.split('').reduce((acc, char) => {
+      return acc + char.charCodeAt(0);
+    }, 0);
+    console.log(' ');
+    console.log('sum charCode',sumCharCode);
+    // 19 is a prime number YOU CAN USE ANY OTHER PRIME VALUE
+    // since the resulted number can go out of size we will do % size
+    const hashKey = (sumCharCode * 19) % this.size;
+    console.log('HASH KEY', hashKey);
+    return hashKey; // 2000
+  }
+  
+  add(key, value) {
+    // we need to get index in the storage (Array)
+    const hash = this.hash(key);
+    // we need to check if there is already value at that index
+    if (!this.storage[hash]) {
+      // if there is no value create linkedlist and prepend in it 
+      const ll = new LinkedList();
+      ll.prepend({ [key]: value});
+      this.storage[hash] = ll; // ===> [3222] = {head:{value:{"key","value"},next:{}}}
+    } else {
+      // we already have a value at that index so just prepend to the front of the linkedList
+      this.storage[hash].prepend({ [key]: value});
+    }
+  }
+  
+  get(key){
+    //get the index of that key in the storage (Array)
+    const index = this.hash(key);
+    //return what value we have of that key index in the storage
+    console.log(this.storage[index]);
+    return this.storage[index];
+  }
+  
+  contains(key){
+    // get the index of that key in the storage (Array)
+    const index = this.hash(key);
+    //return TRUE if the key exists in the table already , FALSE if not
+    if (this.storage[index]){
+      console.log('TRUE',this.storage[index]);
+      return true;
+  
+    }else{
+      console.log('FALSE');
+      return false;
+  
+    }
+  }
+  
+}
+
+const hashmap1 = new Hashmap(4000); 
+
+hashmap1.add('fond', 'enamored');
+hashmap1.add('wrath', 'anger');
+hashmap1.add('outift', 'grab');//////
+hashmap1.add('guide', 'usher');
+
+const hashmap2 = new Hashmap(4000); 
+
+hashmap2.add('fond', 'averse');
+hashmap2.add('wrath', 'delight');
+hashmap2.add('flow', 'jam');///////
+hashmap2.add('guide', 'follow');
+
+console.log(hashmap1);
